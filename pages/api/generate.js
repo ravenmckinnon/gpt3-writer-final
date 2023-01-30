cb3a7d4bf65edd3262c01903ea3fe34cd74ab13c
ravenmckinnon <ravenmckinnon@outlook.com>
2023-01-29 18:16:31 -0600

    git commit --all

---
+++ pages/api/generate.js
@@ -0,0 +1,46 @@
+const basePromptPrefix =
+`write me  10  news headlines in the style of NPR:
+topic:
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Take the topic news headlines below and rank them by seo result performance
+  Topic: ${req.body.userInput}
+
+  News Headlines: ${basePromptOutput.text}
+
+ SEO Performance rankings:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
