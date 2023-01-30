25da1f27239ce234a42b8b005527c8fffb03de2c
ravenmckinnon <ravenmckinnon@outlook.com>
2023-01-29 19:23:27 -0600

    git commit --a

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -20,3 +20,5 @@ const generateAction = async (req, res) => {
   const basePromptOutput = baseCompletion.data.choices.pop();
 
   res.status(200).json({ output: basePromptOutput });
+};
+  export default generateAction;
