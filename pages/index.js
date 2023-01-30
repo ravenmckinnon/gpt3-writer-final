0f88d174dcd6d84b2d19b3b25821fbf357394e41
ravenmckinnon <111833737+ravenmckinnon@users.noreply.github.com>
2023-01-30 00:37:29 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -80,19 +80,6 @@ const callGenerateEndpoint = async () => {
   </div>
 )}
 
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-        
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
     </div>
   );
 };
